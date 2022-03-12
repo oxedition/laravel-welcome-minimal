c5a041a5caabe75350eceb11213e8a166b7d04c2
stockhausen <oxynel@me.com>
2022-03-12 11:33:04 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ use Oxedition\LaravelWelcomeMinimal\Http\Controllers\PageController;
 Route::get('/about', [PageController::class, 'show']);
 Route::get('/faqs', [PageController::class, 'show']);
 Route::get('/support', [PageController::class, 'show']);
-Route::get('/terms-of-service', [PageController::class, 'show']);
-Route::get('/privacy-policy', [PageController::class, 'show']);
+Route::get('/terms', [PageController::class, 'show']);
+Route::get('/privacy', [PageController::class, 'show']);
